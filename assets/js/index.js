d1fc5877d13de71ee83e4cc6e96342d2ac7848f3
Richard Virag <andraderv10@gmail.com>
2022-06-18 11:53:08 -0300

    Canvas banner index

---
+++ assets/js/index.js
@@ -0,0 +1,101 @@
+// Banner Canvas
+
+const canvas = document.getElementById('banner-canvas');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = canvas.clientHeight;
+const particlesArray = [];
+
+window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth;
+})
+
+const mouse = {
+    x: undefined,
+    y: undefined
+}
+
+class Particle {
+    constructor() {
+        // this.x = mouse.x;
+        // this.y = mouse.y;
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.size = Math.random() * 8 + 1;
+        this.speedX = Math.random() * 3 - 1.5;
+        this.speedY = Math.random() * 3 - 1.5;
+    }
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+        if (this.size > 0.2) this.size -= 0.1;
+    }
+    draw() {
+        ctx.fillStyle = 'rgba(255, 255, 255, 0.3)';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 50, 0, Math.PI * 2);
+        ctx.fill();
+    }
+}
+
+function handleParticles() {
+    for (let i = 0; i < particlesArray.length; i++) {
+        particlesArray[i].update();
+        particlesArray[i].draw();
+
+        for (let j = i; j < particlesArray.length; j++) {
+            const dx = particlesArray[i].x - particlesArray[j].x;
+            const dy = particlesArray[i].y - particlesArray[j].y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+
+            if (distance < 100) {
+                ctx.beginPath();
+                ctx.strokeStyle = 'rgba(255, 255, 255, 0.3)';
+                ctx.lineWidth = 2;
+                ctx.moveTo(particlesArray[i].x, particlesArray[i].y);
+                ctx.lineTo(particlesArray[j].x, particlesArray[j].y);
+                ctx.stroke();
+            }
+        }
+
+        if (particlesArray[i].size <= 0.3) {
+            particlesArray.splice(i, 1);
+            i--;
+        }
+    }
+}
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    handleParticles();
+    requestAnimationFrame(animate);
+}
+animate();
+
+function loop() {
+    setTimeout(function () {
+        particlesArray.push(new Particle());
+        loop()
+    }, 5); 
+};
+loop();
+
+// Slide homepage
+
+function showSlideHomepage(index) {
+    let slides = document.getElementsByClassName('slide-index');
+    let dots = document.getElementsByClassName('index-dot');
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[index - 1].style.display = "block";  
+    dots[index - 1].className += " active";
+}
+
+showSlideHomepage(1);
